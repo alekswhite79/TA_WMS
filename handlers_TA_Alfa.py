dfe9f38358b482d2ec08cdff82de8023e4fb137d
alekswhite79 <alekswhit@gmail.com>
2024-07-05 15:06:59 +0300

    1

--- handlers_TA_Alfa.py
+++ handlers_TA_Alfa.py
@@ -328,7 +328,7 @@ def py_OrderList_OnStart(hashMap,_files=None,_data=None):
         
     # records = json.loads(res)
     db = pelicans["TA_WMS"]
-    records = db["OrdersForSelection"].find({"НомерЗаказа":"ТА01489519","ВидЗаказа":"Заказ покупателя"})
+    records = db["OrdersForSelection"].find({"ВидЗаказа":"Заказ покупателя","НомерЗаказа":"ТА01489519"})
     #records = db["GoodsForSelection"].find({"ВидЗаказа":"Заказ покупателя"},{"НомерЗаказа": 1, "Получатель": 1})
 
     # records = db["OrdersForSelection"].all()
