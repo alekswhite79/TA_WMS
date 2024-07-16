496e6b5a41d8caf99940ab48128221db7f350cb2
alekswhite79 <alekswhit@gmail.com>
2024-07-16 17:50:55 +0300

    1

--- handlers_TA_Alfa.py
+++ handlers_TA_Alfa.py
@@ -412,7 +412,8 @@ def py_OrderList_OnStart(hashMap, _files=None, _data=None):
     }
 
     j["customcards"]["cardsdata"] = []
-
+    hashMap.put("structcards", j)
+    android.stop(hashMap)
     db = pelicans["TA_WMS"]
     records = db["OrdersForSelection"].find({"ВидЗаказа": "Заказ покупателя"})
 
