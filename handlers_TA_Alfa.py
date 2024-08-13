05178d949e93df50783e6d34154b3cd66fcd7b4f
alekswhite79 <alekswhit@gmail.com>
2024-08-13 10:48:20 +0300

    1

--- handlers_TA_Alfa.py
+++ handlers_TA_Alfa.py
@@ -1154,7 +1154,9 @@ def py_InsertRecords(hashMap, _files=None, _data=None):
     ТоварыЗагрузить=json.loads(hashMap.get("ТоварыЗагрузить"))
     ЗагруженоТоваров = db["GoodsForSelection"].insert(ТоварыЗагрузить, upsert=True)
 
-    hashMap.put("toast", ЗагруженоЗаказов)
+    hashMap.put("ЗагруженоЗаказов1", ЗагруженоЗаказов)
+    hashMap.put("ЗагруженоТоваров1", ЗагруженоТоваров)
+    android.stop(hashMap)
     # if ЗагруженоЗаказов > 0 and ЗагруженоТоваров > 0:
     #     hashMap.put("Speak", "Загружено "+str(ЗагруженоЗаказов)+" заказов из "+str(ЗагруженоТоваров)+ " товаров")
     # import requests
