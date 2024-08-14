29778ce5500df363a8523269fae70bff5ff1f75e
alekswhite79 <alekswhit@gmail.com>
2024-08-14 16:36:45 +0300

    1

--- handlers_TA_Alfa.py
+++ handlers_TA_Alfa.py
@@ -1163,7 +1163,7 @@ def py_InsertRecords(hashMap, _files=None, _data=None):
                 ТоварыЗагрузить=json.loads(hashMap.get("ТоварыЗагрузить"))
                 ЗагруженоТоваров = db["GoodsForSelection"].insert(ТоварыЗагрузить, upsert=True, session=s)
                 
-                if len(ЗагруженоЗаказов) == hashMap.get("ЗагруженоЗаказов") and len(ЗагруженоТоваров) == hashMap.get("ЗагруженоТоваров"):
+                if len(ЗагруженоЗаказов) == int(hashMap.get("ЗагруженоЗаказов")) and len(ЗагруженоТоваров) == int(hashMap.get("ЗагруженоТоваров")):
                     #Надо сообщить об этом 1С
                     android.stop(hashMap)
                     hashMap.put("RunEvent",json.dumps([{"action": "runasync", 
