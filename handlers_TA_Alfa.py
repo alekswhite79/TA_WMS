5b30f806743434c5973fcfcee7833fac8b047460
alekswhite79 <alekswhit@gmail.com>
2024-09-26 17:07:54 +0300

    1

--- handlers_TA_Alfa.py
+++ handlers_TA_Alfa.py
@@ -1198,9 +1198,17 @@ def py_UploadOrders(hashMap, _files=None, _data=None):
 
 # удаляем выгруженные в 1С заказы (после отработки обработчика 1С)
 def py_DeleteRecords(hashMap, _files=None, _data=None):
+    # Для отладки
+    hashMap.put('VAR_DEBUG', "Точка 1")
+    android.stop(hashMap)
     if hashMap.containsKey("ЗаказыСобранные"):
+        hashMap.put('VAR_DEBUG', "Точка 2")
+        android.stop(hashMap)
         recordsZS=json.loads(hashMap.get("ЗаказыСобранные"))
         for record in recordsZS:
+            hashMap.put('VAR_DEBUG', "Точка 3")
+            hashMap.put("Удаляемая запись", json.dumps(record))
+            android.stop(hashMap)
             db["OrdersForSelection"].delete({"$and": [{"ВидЗаказа": record["ВидЗаказа"]},
                                                 {"НомерЗаказа": record["НомерЗаказа"]}]})
             db["OrdersForSelection"].shrink(s)
