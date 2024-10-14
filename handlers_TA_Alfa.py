1f0efd4dc37e5c5a4772d18b3e71c1472972cfb2
alekswhite79 <alekswhit@gmail.com>
2024-10-14 17:59:14 +0300

    1

--- handlers_TA_Alfa.py
+++ handlers_TA_Alfa.py
@@ -1299,7 +1299,7 @@ def py_InsertRecords(hashMap, _files=None, _data=None):
                 
                 if len(ЗагруженоЗаказов) == int(hashMap.get("ЗагруженоЗаказов")) and len(ЗагруженоТоваров) == int(hashMap.get("ЗагруженоТоваров")):
                     #Надо сообщить об этом 1С
-                    hashMap.put("RunEvent",json.dumps([{"action": "runasync", "type": "online", "method": "ДанныеВТСДЗагружены"}]))
+                    hashMap.put("RunEvent",json.dumps([{"action": "run", "type": "online", "method": "ДанныеВТСДЗагружены"}]))
                     # hashMap.put("speak","Загружены новые заказы")                    
                     # hashMap.remove("ЗаказыЗагрузить")    
                     # hashMap.remove("ТоварыЗагрузить")    
