dc585afc0b4f9bb33ab7fe1435d8bbec02bb49ff
alekswhite79 <alekswhit@gmail.com>
2024-10-19 17:26:05 +0300

    1

--- handlers_TA_Alfa.py
+++ handlers_TA_Alfa.py
@@ -824,7 +824,7 @@ def Display_Elrment(hashMap):
 # при вводе в экране Отбор
 def py_select_on_input(hashMap, _files=None, _data=None): 
 
-    # android.stop(hashMap)
+    android.stop(hashMap)
 
     if hashMap.get("listener") == 'barcode': #сканирование
 
