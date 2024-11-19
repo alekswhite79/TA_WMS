356b2d3a909888077ec40c26bc7babd581ee4ca5
alekswhite79 <alekswhit@gmail.com>
2024-11-19 09:49:18 +0200

    1

--- handlers_TA_Alfa.py
+++ handlers_TA_Alfa.py
@@ -1645,6 +1645,6 @@ def check_pin(hashMap, _files=None, _data=None):
         hashMap.put("ShowScreen", "Выбор операции")
     else:
         hashMap.put("toast", "Неверный PIN")
-    # android.stop(hashMap)
+    android.stop(hashMap)
 
     return hashMap
