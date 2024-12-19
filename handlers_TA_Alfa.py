df48e11d8778a728b0718389134db1f4a69dc4ae
alekswhite79 <alekswhit@gmail.com>
2024-12-19 17:02:20 +0200

    1

--- handlers_TA_Alfa.py
+++ handlers_TA_Alfa.py
@@ -1660,7 +1660,7 @@ def py_auth_on_start(hashMap, _files=None, _data=None):
 # при вводе в экране Авторизация
 def py_auth_on_input(hashMap, _files=None, _data=None):
 
-    # android.stop(hashMap)
+    android.stop(hashMap)
     hashMap.put("noRefresh","") 
     if hashMap.get("listener") == 'кнОК':  # нажата кнопка ОК
 
