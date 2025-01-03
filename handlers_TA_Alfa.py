850505534121add7d7efd5c62629154f4f801755
alekswhite79 <alekswhit@gmail.com>
2025-01-03 12:58:13 +0200

    1

--- handlers_TA_Alfa.py
+++ handlers_TA_Alfa.py
@@ -257,13 +257,14 @@ def py_LoadGoods(hashMap):
 
     if len(records) > 0:
         # hashMap.put("Records", json.dumps(records))
-        # hashMap.put("ДляОтладки", "Количество найденых записей >0")
+        hashMap.put("toast", "Количество найденых записей >0")
         # android.stop(hashMap)
         for record in records:
             if record['СпланированноеКоличество'] == record['ФактическоеКоличество']: # не добавлять карточку если позиция собрана
                 continue
             if record['Исполнитель'] is not None and record['Исполнитель'] != hashMap.get('user'): # не добавлять карточку если позиция собирается другим исполнителем
                 continue
+            hashMap.put("toast", "Количество найденых записей >0")
             c = {
                 "key": record['Код'],
                 "ЗаказаноОтобрано": "<font color=#F08080>"+str(record['ФактическоеКоличество'])+"/"+str(record['СпланированноеКоличество'])+" " + record['ЕдиницаИзмерения']+"</font>",
